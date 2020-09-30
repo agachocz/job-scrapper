5235b4b94468df7be1e960cf01df22a652c339b2
agachocz <agachocz@gmail.com>
2020-09-30 14:18:37 +0200

    Refactoring.

--- analysis.R
+++ analysis.R
@@ -3,8 +3,8 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 
-data_scientist <- read.table("data_scientist_offers.csv", colClasses = c("character", "character"))
-data_analyst <- read.table("data_analyst_offers.csv", colClasses = c("character", "character"))
+data_scientist <- read.table("data scientist offers.csv", colClasses = c("character", "character"))
+data_analyst <- read.table("data analyst offers.csv", colClasses = c("character", "character"))
 
 all_data <- rbind(data_scientist, data_analyst)
 
@@ -22,25 +22,32 @@ plot_data <- all_data_stats %>% select(R, python, degree) %>%
   pivot_longer(everything(), names_to = "req")
 
 
+# Charts
 
-plot_data %>% filter(str_detect(req, "R_")) %>% mutate(ypos = cumsum(value)- 0.5*value) %>%
+# Required R
+plot_data %>% filter(str_detect(req, "R_")) %>% 
+  mutate(ypos = cumsum(value)- 0.5*value) %>%
   ggplot(aes(x = "",y = value, fill = req)) + 
   geom_bar(stat = "identity", color = "white") + coord_polar("y") + theme_void() + 
   geom_text(aes(y = ypos, label = scales::percent(value/nrow(all_data_stats), accuracy = 0.1)),
           color = "white", size=6) +
   theme(legend.position="none")
 
-plot_data %>% filter(str_detect(req, "py")) %>% mutate(ypos = cumsum(value)- 0.5*value) %>%
+# Required Python
+plot_data %>% filter(str_detect(req, "py")) %>% 
+  mutate(ypos = cumsum(value)- 0.5*value) %>% # positions of text labels
   ggplot(aes(x = "",y = value, fill = req)) + 
   geom_bar(stat = "identity", color = "white") + coord_polar("y") + theme_void() + 
   geom_text(aes(y = ypos, label = scales::percent(value/nrow(all_data_stats), accuracy = 0.1)), 
             color = "white", size=6) +
   theme(legend.position="none")
 
+# Required degree
 plot_data %>% filter(str_detect(req, "deg")) %>% 
-  mutate(ypos = cumsum(value) - 0.5*value) %>%
+  mutate(ypos = cumsum(value) - 0.5*value) %>% # positions of text labels
   ggplot(aes(x = "",y = value, fill = req)) + 
-  geom_bar(stat = "identity", color = "white") + coord_polar("y", direction = -1) + theme_void() + 
+  geom_bar(stat = "identity", color = "white") + 
+  coord_polar("y", direction = -1) + theme_void() + # anti-clockwise direction
   geom_text(aes(y = ypos, label = scales::percent(value/nrow(all_data_stats), accuracy = 0.1)), 
             color = "white", size=6) +
   theme(legend.position="none")
--- scrapping.R
+++ scrapping.R
@@ -2,39 +2,43 @@ install.packages("RSelenium")
 library(RSelenium)
 library(stringr)
 
-
+# searched keywords
 job_titles <- c("data scientist", "data analyst")
 
-
-
-# uruchamianie
-binman::list_versions("chromedriver")
-
+# running a driver
 remote <- rsDriver(browser = "firefox")
 client <- remote[["client"]]
-
 client$navigate("https://www.pracuj.pl/")
 
-keywordInput <- client$findElement(using = "css selector", "[data-test=autocomplete-kw-input]")
-searchButton <- client$findElement(using = "css selector", "[data-test=form-submit]")
 
-# wyszukiwanie ofert
-kw_input <- client$findElement(using = "css selector", "[data-test=autocomplete-kw-input]")$
-  findChildElements(using = "tag name", "input")
-
-kw_input[[1]]$sendKeysToElement(list(job_titles[[1]]))
-kw_input[[1]]$submitElement()
-
-# zbieranie danych
-offers_list <- client$findElement(using = "class name", "results__list-container")$
-  findChildElements(using = "class name", "offer-details__title-link")
-
-offers_links <- list()
-
-for(i in 1:length(offers_list)){
- offers_links[[i]] <- unlist(offers_list[[i]]$getElementAttribute("href"))
+# Scrapping offers' list
+search_for_offers <- function(keyword){
+  
+  # offers' list
+  kw_input <- client$findElement(using = "css selector", "[data-test=autocomplete-kw-input]")$
+    findChildElements(using = "tag name", "input")
+  
+  kw_input[[1]]$sendKeysToElement(list(keyword))
+  kw_input[[1]]$submitElement()
+  
+  # collecting data
+  offers_list <- client$findElement(using = "class name", "results__list-container")$
+    findChildElements(using = "class name", "offer-details__title-link")
+  
+  offers_links <- list()
+  
+  for(i in 1:length(offers_list)){
+    offers_links[[i]] <- unlist(offers_list[[i]]$getElementAttribute("href"))
+  }
+  
+  # filtering off nulls
+  offers_links = offers_links[-which(sapply(offers_links, is.null))]
+  
+  return(offers_links)
 }
 
+
+# Scrapping job requirements
 get_requirements <- function(offer_link){
   client$navigate(offer_link)
   Sys.sleep(2)
@@ -49,54 +53,37 @@ get_requirements <- function(offer_link){
   return(c(link = offer_link, descr = unlist(descr)))
 }
 
-offers_links = offers_links[-which(sapply(offers_links, is.null))]
-offers_data <- sapply(offers_links, get_requirements)
-offers_data <- offers_data[-which(sapply(offers_data, function(x){length(x)<2}))]
-
-offers_dt <- unlist(offers_data, recursive = F)
-i <- 1:length(offers_dt)
-offers_dt <- data.frame(link = offers_dt[i%%2 == 1], descr = offers_dt[i%%2 == 0])
-
-
-offers_dt$descr <- trimws(offers_dt$descr, which = "both")
-offers_dt$link <- as.character(offers_dt$link)
-
-write.table(offers_dt, "data_scientist_offers.csv")
-
-
-
-
-
-
-
-
-kw_input[[1]]$sendKeysToElement(list(job_titles[[2]]))
-kw_input[[1]]$submitElement()
-
-# zbieranie danych
-offers_list <- client$findElement(using = "class name", "results__list-container")$
-  findChildElements(using = "class name", "offer-details__title-link")
-
-offers_links <- list()
-
-for(i in 1:length(offers_list)){
-  offers_links[[i]] <- unlist(offers_list[[i]]$getElementAttribute("href"))
+# Formatting nad saving data
+save_data <- function(offers_data, file_name){
+  
+  # don't count records without description element
+  offers_data <- offers_data[-which(sapply(offers_data, function(x){length(x)<2}))]
+  
+  # turn into data frame
+  offers_dt <- unlist(offers_data, recursive = F)
+  i <- 1:length(offers_dt)
+  offers_dt <- data.frame(link = offers_dt[i%%2 == 1], descr = offers_dt[i%%2 == 0])
+  
+  # get rid off spaces
+  offers_dt$descr <- trimws(offers_dt$descr, which = "both")
+  offers_dt$link <- as.character(offers_dt$link)
+  
+  # save file
+  write.table(offers_dt, file_name)
+  
 }
 
-offers_links = offers_links[-which(sapply(offers_links, is.null))]
+
+offers_links <- search_for_offers(job_titles[[1]])
+offers_data <- sapply(offers_links, get_requirements)
+save_data(offers_data, paste(job_titles[[1]],"offers.csv"))
+
+
+offers_links <- search_for_offers(job_titles[[2]])
 offers_data_2 <- sapply(offers_links, get_requirements)
-offers_data_2 <- offers_data_2[-which(sapply(offers_data_2, function(x){length(x)<2}))]
-
-offers_dt_2 <- unlist(offers_data_2, recursive = F)
-i <- 1:length(offers_dt_2)
-offers_dt_2 <- data.frame(link = offers_dt_2[i%%2 == 1], descr = offers_dt_2[i%%2 == 0])
-
-offers_dt_2$descr <- trimws(offers_dt_2$descr, which = "both")
-offers_dt_2$link <- as.character(offers_dt_2$link)
-
-write.table(offers_dt_2, "data_analyst_offers.csv")
+save_data(offers_data_2, paste(job_titles[[2]],"offers.csv"))
 
 
+# close the driver and stop the selenium server
 client$close()
-# stop the selenium server
 remote[["server"]]$stop()
